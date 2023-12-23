10355a19a0bfce9dc6621f49a4900b3ea1d30dd3
JoshuaDevs-E <91708533+JoshuaDevs-E@users.noreply.github.com>
2023-12-23 00:03:02 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <title>{metadata.title}</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="language" content="Español" />
         <meta name="author" content="Joshua Peña" />
